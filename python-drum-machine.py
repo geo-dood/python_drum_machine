ad263836bc517c4cc6df7eaca218f1f540164d46
George Maysack-Schlueter <georgemaysack13@gmail.com>
2022-06-03 12:46:18 -0500

    Updated colors, added a scrolling playback indicator, added drum sound folder to reference later, rearraged instrument order/naming, removed crash symbol and added open/close hi-hat

--- python-drum-machine.py
+++ python-drum-machine.py
@@ -21,9 +21,10 @@ HEIGHT = 800
 black = (0, 0, 0)
 white = (255, 255, 255)
 gray = (128, 128, 128)
-red = (208, 22, 22)
+red = (174, 12, 12)
 dark_gray = (88, 83, 83)
 cream = (255, 253, 208)
+bright_red = (255, 58, 58)
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Simple Python Drum Machine by George Maysack')
@@ -35,24 +36,30 @@ beats = 8
 instruments = 6
 boxes = []
 clicked = [[-1 for _ in range(beats)] for _ in range(instruments)]
+bpm = 240
+playing = True
+active_length = 0
+active_beat = 0
+beat_changed = True
 
-def draw_grid(clicks):
+
+def draw_grid(clicks, beat):
     left_box = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT - 200], 5)
     bottom_box = pygame.draw.rect(screen, gray, [0, HEIGHT - 200, WIDTH, 200], 5)
     boxes = []
     colors = [gray, white, gray]
-    hi_hat_text = caption_font.render('Hi-Hat', True, black)
-    screen.blit(hi_hat_text, (30, 30))
     snare_text = caption_font.render('Snare', True, black)
-    screen.blit(snare_text, (30, 130))
+    screen.blit(snare_text, (10, 30))
     kick_text = caption_font.render('Kick', True, black)
-    screen.blit(kick_text, (30, 230))
-    crash_text = caption_font.render('Crash', True, black)
-    screen.blit(crash_text, (30, 330))
-    clap_text = caption_font.render('Clap', True, black)
-    screen.blit(clap_text, (30, 430))
+    screen.blit(kick_text, (10, 130))
     tom_text = caption_font.render('Tom', True, black)
-    screen.blit(tom_text, (30, 530))
+    screen.blit(tom_text, (10, 230))
+    closed_hat_text = caption_font.render('Closed Hat', True, black)
+    screen.blit(closed_hat_text, (10, 330))
+    open_hat_text = caption_font.render('Open Hat', True, black)
+    screen.blit(open_hat_text, (10, 430))
+    clap_text = caption_font.render('Clap', True, black)
+    screen.blit(clap_text, (10, 530))
     for i in range(instruments):
         pygame.draw.line(screen, gray, (0, (i * 100) + 100), (200, (i * 100) + 100), 3)
 
@@ -72,6 +79,8 @@ def draw_grid(clicks):
                              [i * ((WIDTH - 200) // beats) + 200, (j * 100), ((WIDTH - 200) // beats),
                               ((HEIGHT - 200) // instruments)], 2, 5)
             boxes.append((rect, (i, j)))
+
+        active = pygame.draw.rect(screen, bright_red, [beat * ((WIDTH - 200)//beats) + 200, 0, ((WIDTH - 200)//beats), instruments * 100], 5, 3,)
     return boxes
 
 
@@ -79,7 +88,7 @@ run = True
 while run:
     timer.tick(fps)
     screen.fill(cream)
-    boxes = draw_grid(clicked)
+    boxes = draw_grid(clicked, active_beat)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -90,7 +99,21 @@ while run:
                     coords = boxes[i][1]
                     clicked[coords[1]][coords[0]] *= -1
 
+    beat_length = 3600 // bpm
+
+    if playing:
+        if active_length < beat_length:
+            active_length += 1
+        else:
+            active_length = 0
+            if active_beat < beats -1:
+                active_beat += 1
+                beat_changed = True
+            else:
+                active_beat = 0
+                beat_changed = 0
+
     pygame.display.flip()
 pygame.quit()
 
-# REACHED 37:15 IN TUTORIAL!!!
+# REACHED 49:00 IN TUTORIAL!!!
